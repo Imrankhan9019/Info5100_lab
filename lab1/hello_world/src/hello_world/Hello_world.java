7c4dfeb2ebc73a99aaa2c94f54783d6ee9e8646a
Imran <pathan.i@northeastern.edu>
2024-09-20 16:10:57 -0400

    java code for hello world

---
+++ lab1/hello_world/src/hello_world/Hello_world.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello_world;
+
+/**
+ *
+ * @author ACER
+ */
+public class Hello_world {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+        // TODO code application logic here
+    }
+    
+}
